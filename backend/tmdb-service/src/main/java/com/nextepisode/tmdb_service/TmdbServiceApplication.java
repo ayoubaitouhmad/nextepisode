3b88ba9d4d6cc1308d8a131a02a2da861d9b8a77
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-11-10 23:23:47 +0100

    git commit -m "feat(tmdb-service): create the service"

---
+++ backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/TmdbServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nextepisode.tmdb_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmdbServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmdbServiceApplication.class, args);
+	}
+
+}
