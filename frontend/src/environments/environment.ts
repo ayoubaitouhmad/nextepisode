65e02bc809044240c02e6817351c3587d154366c
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-11-30 18:26:22 +0100

    feat(angular): refactor the api url

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   tmdbApiKey: '8081565fa90c030dacafd8bc7240884f',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p',
-  apiUrl: 'http://localhost:8081/api/v1/auth/register',
+  apiUrl: 'http://localhost:8081/api/v1/',
 };
