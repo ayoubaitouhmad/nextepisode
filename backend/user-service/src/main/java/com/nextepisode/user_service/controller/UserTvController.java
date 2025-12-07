c706de36cf66cba60a1b12b3f768ef0584a1d5f7
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-07 23:29:29 +0100

    feat(user): Add user tv controller

---
+++ backend/user-service/src/main/java/com/nextepisode/user_service/controller/UserTvController.java
@@ -0,0 +1,38 @@
+package com.nextepisode.user_service.controller;
+
+import com.nextepisode.user_service.config.ApiPaths;
+import com.nextepisode.user_service.dto.MovieListResponse;
+import com.nextepisode.user_service.dto.TvListResponse;
+import com.nextepisode.user_service.dto.UserMovieTvStats;
+import com.nextepisode.user_service.service.UserService;
+import com.nextepisode.user_service.service.UserTvService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(ApiPaths.BASE + "/tvs")
+@RequiredArgsConstructor
+public class UserTvController {
+
+    private final UserTvService userTvService;
+
+    @GetMapping("/favorite")
+    public TvListResponse favoriteMovies(
+            @AuthenticationPrincipal String username,
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size
+    ) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("um.createdAt").descending());
+        return userTvService.getUserFavoriteMovies(username, pageable);
+    }
+
+
+
+}
