8443f5506b6ab184d0731c165c0389fdf9d6a0a5
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-08 21:36:55 +0100

    feature(angular): Implement user watched movies and tv shows

--- frontend/src/app/features/user/pages/profile/components/watched/watched.component.ts
+++ frontend/src/app/features/user/pages/profile/components/watched/watched.component.ts
@@ -4,6 +4,8 @@ import {TvCardComponent} from '../shared/tv-card/tv-card.component';
 import {UserMovieList} from '../../../../../../core/models/user/movie/movie.model';
 import {UserTvShowList} from '../../../../../../core/models/user/tv/tv.model';
 import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
+import {UserMovieService} from '../../../../../../core/services/user/movie/user-movie.service';
+import {UserTvService} from '../../../../../../core/services/user/tv/user-tv.service';
 
 @Component({
   selector: 'app-watched',
@@ -15,9 +17,15 @@ import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
 export class WatchedComponent implements OnInit {
   watchedMovies: UserMovieList = {page: 0, totalPages: 0, results: [], totalResults: 0};
   watchedTvShows: UserTvShowList = {page: 0, totalPages: 0, results: [], totalResults: 0};
+  loading: boolean = false;
+  errorMessage: string = '';
 
-  loading = false;
-  errorMessage = '';
+  public constructor(
+    private userMovieService: UserMovieService,
+    private userTvService: UserTvService
+  ) {
+  }
+
 
   ngOnInit(): void {
     this.loadWatched();
@@ -26,10 +34,31 @@ export class WatchedComponent implements OnInit {
   loadWatched(): void {
     this.loading = true;
     setTimeout(() => {
+      this.loadWatchedTvShows();
+      this.loadWatchedMovies();
 
       this.loading = false;
     }, 300);
   }
+
+  loadWatchedMovies(): void {
+    this.userMovieService.getUserWatchedMovies().subscribe({
+      next: (movies) => {
+        this.watchedMovies = movies;
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  loadWatchedTvShows(): void {
+    this.userTvService.getUserWatchedTvShows().subscribe({
+      next: (tvShows) => {
+        this.watchedTvShows = tvShows;
+      },
+      error: err => console.log(err)
+    });
+  }
+
 
   hasContent(): boolean {
     return this.watchedMovies.results.length > 0 ||
