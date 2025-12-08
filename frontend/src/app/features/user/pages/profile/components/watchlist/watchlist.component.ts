06683f0976571d7d17ee6d218993aacc18787e03
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-08 21:54:31 +0100

    feature(angular): Implement user watch list movies and tv shows

--- frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
+++ frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit} from '@angular/core';
 import {NgFor, NgIf} from '@angular/common';
-import {GroupedMovies, groupMoviesByType, MovieDto, STATIC_WATCHLIST_MOVIES} from '../movie.model';
 import {TvCardComponent} from '../shared/tv-card/tv-card.component';
 import {UserMovieList} from '../../../../../../core/models/user/movie/movie.model';
 import {UserTvShowList} from '../../../../../../core/models/user/tv/tv.model';
 import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
+import {UserMovieService} from '../../../../../../core/services/user/movie/user-movie.service';
+import {UserTvService} from '../../../../../../core/services/user/tv/user-tv.service';
 
 @Component({
   selector: 'app-watchlist',
@@ -20,6 +21,14 @@ export class WatchlistComponent implements OnInit {
   loading = false;
   errorMessage = '';
 
+  public constructor(
+    private userMovieService: UserMovieService,
+    private userTvService: UserTvService
+  ) {
+  }
+
+
+
   ngOnInit(): void {
     this.loadWatchlist();
   }
@@ -27,9 +36,28 @@ export class WatchlistComponent implements OnInit {
   loadWatchlist(): void {
     this.loading = true;
     setTimeout(() => {
-
+      this.loadInWatchListMovies();
+      this.loadInWatchListTvShows();
       this.loading = false;
     }, 300);
+  }
+
+  loadInWatchListMovies(): void {
+    this.userMovieService.getUserInWatchListMovies().subscribe({
+      next: (movies) => {
+        this.watchListMovies = movies;
+      },
+      error: err => console.log(err)
+    });
+  }
+
+  loadInWatchListTvShows(): void {
+    this.userTvService.getUserInWatchListTvShows().subscribe({
+      next: (tvShows) => {
+        this.watchListTvShows = tvShows;
+      },
+      error: err => console.log(err)
+    });
   }
 
   hasContent(): boolean {
