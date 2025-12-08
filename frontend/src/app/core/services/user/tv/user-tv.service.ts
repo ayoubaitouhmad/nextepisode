715e4bdcd47f1725e8447eb81cfd5d5ee6d9ca7e
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-08 20:14:18 +0100

    feat(angular): Add user tv service

---
+++ frontend/src/app/core/services/user/tv/user-tv.service.ts
@@ -0,0 +1,49 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Service} from '../service';
+import {Observable} from 'rxjs';
+import {UserMoviesAndTvShowStats} from '../../../models/shared/shared-dtos';
+import {UserTvShowList} from '../../../models/tv/tv.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserTvService extends Service {
+  protected apiUrl: string;
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.apiUrl = `${this.baseServiceApiUrl}/tvs`;
+  }
+
+
+  /**
+   * Get user's tv shows statistics (counts by category)
+   */
+  getUserTvShowsStatistics(): Observable<UserMoviesAndTvShowStats> {
+    return this.http.get<UserMoviesAndTvShowStats>(`${this.apiUrl}/stats`);
+  }
+
+  /**
+   * Get user's tv shows statistics (counts by category)
+   */
+  getUserFavoriteTvShows(): Observable<UserTvShowList> {
+    return this.http.get<UserTvShowList>(`${this.apiUrl}/favorite`);
+  }
+
+  /**
+   * Get user's tv shows statistics (counts by category)
+   */
+  getUserWatchedTvShows(): Observable<UserTvShowList> {
+    return this.http.get<UserTvShowList>(`${this.apiUrl}/watched`);
+  }
+
+  /**
+   * Get user's tv shows statistics (counts by category)
+   */
+  getUserInWatchListTvShows(): Observable<UserTvShowList> {
+    return this.http.get<UserTvShowList>(`${this.apiUrl}/watchlist`);
+  }
+
+
+}
