cad2988aba40a32b11ed0ac9678e12d62e86e8e0
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-08 21:05:52 +0100

    fix(angular): fix wrong WatchedComponent implementation

--- frontend/src/app/features/user/pages/profile/components/watched/watched.component.ts
+++ frontend/src/app/features/user/pages/profile/components/watched/watched.component.ts
@@ -1,23 +1,21 @@
 import {Component, OnInit} from '@angular/core';
 import {NgFor, NgIf} from '@angular/common';
-import {
-  MovieDto,
-  GroupedMovies,
-  STATIC_WATCHED_MOVIES,
-  groupMoviesByType
-} from '../movie.model';
 import {TvCardComponent} from '../shared/tv-card/tv-card.component';
+import {UserMovieList} from '../../../../../../core/models/user/movie/movie.model';
+import {UserTvShowList} from '../../../../../../core/models/user/tv/tv.model';
+import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-watched',
   standalone: true,
-  imports: [NgFor, NgIf, TvCardComponent],
+  imports: [NgFor, NgIf, TvCardComponent, MovieCardComponent],
   templateUrl: './watched.component.html',
   styleUrls: ['./watched.component.scss']
 })
 export class WatchedComponent implements OnInit {
-  watchedMovies: MovieDto[] = [];
-  watchedMoviesGrouped: GroupedMovies = {movies: [], tvSeries: []};
+  watchedMovies: UserMovieList = {page: 0, totalPages: 0, results: [], totalResults: 0};
+  watchedTvShows: UserTvShowList = {page: 0, totalPages: 0, results: [], totalResults: 0};
+
   loading = false;
   errorMessage = '';
 
@@ -28,14 +26,13 @@ export class WatchedComponent implements OnInit {
   loadWatched(): void {
     this.loading = true;
     setTimeout(() => {
-      this.watchedMovies = STATIC_WATCHED_MOVIES;
-      this.watchedMoviesGrouped = groupMoviesByType(STATIC_WATCHED_MOVIES);
+
       this.loading = false;
     }, 300);
   }
 
   hasContent(): boolean {
-    return this.watchedMoviesGrouped.movies.length > 0 ||
-      this.watchedMoviesGrouped.tvSeries.length > 0;
+    return this.watchedMovies.results.length > 0 ||
+      this.watchedTvShows.results.length > 0;
   }
 }
