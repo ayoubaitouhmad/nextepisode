8bd0b1d856e78ae2ac09895817d6d85004db501a
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-08 21:06:04 +0100

    fix(angular): fix wrong WatchlistComponent implementation

--- frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
+++ frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
@@ -2,17 +2,21 @@ import {Component, OnInit} from '@angular/core';
 import {NgFor, NgIf} from '@angular/common';
 import {GroupedMovies, groupMoviesByType, MovieDto, STATIC_WATCHLIST_MOVIES} from '../movie.model';
 import {TvCardComponent} from '../shared/tv-card/tv-card.component';
+import {UserMovieList} from '../../../../../../core/models/user/movie/movie.model';
+import {UserTvShowList} from '../../../../../../core/models/user/tv/tv.model';
+import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
 
 @Component({
   selector: 'app-watchlist',
   standalone: true,
-  imports: [NgFor, NgIf, TvCardComponent],
+  imports: [NgFor, NgIf, TvCardComponent, MovieCardComponent],
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.scss']
 })
 export class WatchlistComponent implements OnInit {
-  watchlistMovies: MovieDto[] = [];
-  watchlistMoviesGrouped: GroupedMovies = {movies: [], tvSeries: []};
+  watchListMovies: UserMovieList = {page: 0, totalPages: 0, results: [], totalResults: 0};
+  watchListTvShows: UserTvShowList = {page: 0, totalPages: 0, results: [], totalResults: 0};
+
   loading = false;
   errorMessage = '';
 
@@ -23,14 +27,13 @@ export class WatchlistComponent implements OnInit {
   loadWatchlist(): void {
     this.loading = true;
     setTimeout(() => {
-      this.watchlistMovies = STATIC_WATCHLIST_MOVIES;
-      this.watchlistMoviesGrouped = groupMoviesByType(STATIC_WATCHLIST_MOVIES);
+
       this.loading = false;
     }, 300);
   }
 
   hasContent(): boolean {
-    return this.watchlistMoviesGrouped.movies.length > 0 ||
-      this.watchlistMoviesGrouped.tvSeries.length > 0;
+    return this.watchListMovies.results.length > 0 ||
+      this.watchListTvShows.results.length > 0;
   }
 }
