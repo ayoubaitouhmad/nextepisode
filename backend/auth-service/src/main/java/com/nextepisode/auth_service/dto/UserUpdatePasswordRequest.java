7f0e0464eb4ed505c7137dee8f2f4e4fe2dd8f74
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-10 22:06:18 +0100

    feat(auth): create the UserUpdatePasswordRequest dto

---
+++ backend/auth-service/src/main/java/com/nextepisode/auth_service/dto/UserUpdatePasswordRequest.java
@@ -0,0 +1,20 @@
+package com.nextepisode.auth_service.dto;
+
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UserUpdatePasswordRequest(
+        @NotBlank(message = "Current password is required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        String currentPassword,
+
+        @NotBlank(message = "New password is required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        String newPassword,
+
+        @NotBlank(message = "Confirm password is required")
+        @Size(min = 8, max = 20, message = "Password must be between 8 and 20 characters")
+        String confirmPassword
+) {
+}
