65515897ce8ac3af0f3cb4ab7a3606832dbd9896
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-12 17:51:33 +0100

    feat(angular):add pagination to the in watch list  tab

--- frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
+++ frontend/src/app/features/user/pages/profile/components/watchlist/watchlist.component.ts
@@ -6,11 +6,12 @@ import {UserTvShowList} from '../../../../../../core/models/user/tv/tv.model';
 import {MovieCardComponent} from '../shared/movie-card/movie-card.component';
 import {UserMovieService} from '../../../../../../core/services/user/movie/user-movie.service';
 import {UserTvService} from '../../../../../../core/services/user/tv/user-tv.service';
+import {ListPaginationComponent} from '../shared/list-pagination/list-pagination.component';
 
 @Component({
   selector: 'app-watchlist',
   standalone: true,
-  imports: [NgFor, NgIf, TvCardComponent, MovieCardComponent],
+  imports: [NgFor, NgIf, TvCardComponent, MovieCardComponent, ListPaginationComponent],
   templateUrl: './watchlist.component.html',
   styleUrls: ['./watchlist.component.scss']
 })
@@ -18,8 +19,14 @@ export class WatchlistComponent implements OnInit {
   watchListMovies: UserMovieList = {page: 0, total_pages: 0, results: [], total_results: 0};
   watchListTvShows: UserTvShowList = {page: 0, total_pages: 0, results: [], total_results: 0};
 
+  watchListMoviesToShow: UserMovieList = {page: 0, total_pages: 0, results: [], total_results: 0};
+  watchListTvShowsToShow: UserTvShowList = {page: 0, total_pages: 0, results: [], total_results: 0};
+
   loading = false;
   errorMessage = '';
+
+  moviesDisplayCount: number = 5;
+  tvShowsDisplayCount: number = 5;
 
   public constructor(
     private userMovieService: UserMovieService,
@@ -46,6 +53,12 @@ export class WatchlistComponent implements OnInit {
     this.userMovieService.getUserInWatchListMovies().subscribe({
       next: (movies) => {
         this.watchListMovies = movies;
+        this.watchListMoviesToShow = {
+          page: this.watchListMovies.page,
+          total_pages: this.watchListMovies.total_pages,
+          results: this.watchListMovies.results.slice(0, this.moviesDisplayCount),
+          total_results: this.watchListMovies.total_results
+        };
       },
       error: err => console.log(err)
     });
@@ -55,6 +68,12 @@ export class WatchlistComponent implements OnInit {
     this.userTvService.getUserInWatchListTvShows().subscribe({
       next: (tvShows) => {
         this.watchListTvShows = tvShows;
+        this.watchListTvShowsToShow = {
+          page: this.watchListTvShows.page,
+          total_pages: this.watchListTvShows.total_pages,
+          results: this.watchListTvShows.results.slice(0, this.tvShowsDisplayCount),
+          total_results: this.watchListTvShows.total_results
+        };
       },
       error: err => console.log(err)
     });
@@ -63,5 +82,25 @@ export class WatchlistComponent implements OnInit {
   hasContent(): boolean {
     return this.watchListMovies.results.length > 0 ||
       this.watchListTvShows.results.length > 0;
+  }
+
+  showMoreMovies(): void {
+    if (this.watchListMoviesToShow.results.length >= this.watchListMovies.results.length) return;
+    this.watchListMoviesToShow.results = this.watchListMovies.results.slice(0, this.watchListMoviesToShow.results.length + this.moviesDisplayCount);
+  }
+
+  showLessMovies(): void {
+    if (this.watchListMoviesToShow.results.length <= this.moviesDisplayCount) return;
+    this.watchListMoviesToShow.results = this.watchListMovies.results.slice(0, this.watchListMoviesToShow.results.length - this.moviesDisplayCount);
+  }
+
+  showMoreTvShows(): void {
+    if (this.watchListTvShowsToShow.results.length >= this.watchListTvShows.results.length) return;
+    this.watchListTvShowsToShow.results = this.watchListTvShows.results.slice(0, this.watchListTvShowsToShow.results.length + this.tvShowsDisplayCount);
+  }
+
+  showLessTvShows(): void {
+    if (this.watchListTvShowsToShow.results.length <= this.tvShowsDisplayCount) return;
+    this.watchListTvShowsToShow.results = this.watchListTvShows.results.slice(0, this.watchListTvShowsToShow.results.length - this.tvShowsDisplayCount);
   }
 }
