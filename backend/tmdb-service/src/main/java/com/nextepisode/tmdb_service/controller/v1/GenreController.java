1c0f6b80f361ab3e4d5e748e23cafbe15fb8f66e
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-13 15:02:23 +0100

    feat(tmdb): support genres endpoints

---
+++ backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/controller/v1/GenreController.java
@@ -0,0 +1,32 @@
+package com.nextepisode.tmdb_service.controller.v1;
+
+import com.nextepisode.tmdb_service.config.ApiPaths;
+import com.nextepisode.tmdb_service.dto.TMDBGenreListResponse;
+import com.nextepisode.tmdb_service.service.TMDBGenreService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequestMapping(ApiPaths.API_V1 + "/genres")
+@RestController
+public class GenreController {
+
+    private final TMDBGenreService tmdbGenreService;
+
+    public GenreController(TMDBGenreService tmdbGenreService) {
+        this.tmdbGenreService = tmdbGenreService;
+    }
+
+    @GetMapping("/movie-genres")
+    public TMDBGenreListResponse moviesGenres() {
+        return tmdbGenreService.getMovieGenres();
+    }
+    @GetMapping("/tv-show-genres")
+    public TMDBGenreListResponse tvShowGenres() {
+        return tmdbGenreService.getTvShowGenres();
+    }
+
+
+}
