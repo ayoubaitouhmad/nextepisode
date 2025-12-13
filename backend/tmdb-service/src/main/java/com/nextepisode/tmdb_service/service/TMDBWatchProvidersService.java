874e2151cd9233ffd3bd1ff5ab894b2b2a58e364
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-13 17:47:24 +0100

    feat(tmdb): Support watch providers - add getWatchProvidersForMovies,getWatchProvidersForTvShows to TMDBWatchProvidersService - add watch providers endpoints

--- backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/service/TMDBWatchProvidersService.java
+++ backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/service/TMDBWatchProvidersService.java
@@ -1,11 +1,14 @@
 package com.nextepisode.tmdb_service.service;
 
-import com.nextepisode.tmdb_service.dto.movie.response.TMDBCountryListResponse;
+
+import com.nextepisode.tmdb_service.dto.TMDBCountryListResponse;
+import com.nextepisode.tmdb_service.dto.TMDBWatchProviderListResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -15,7 +18,6 @@ public class TMDBWatchProvidersService extends BaseService {
     public TMDBWatchProvidersService(RestClient TMDBClient) {
         super(TMDBClient);
     }
-
 
 
     @Cacheable("countries")
@@ -35,4 +37,65 @@ public class TMDBWatchProvidersService extends BaseService {
             throw new RuntimeException("Failed to get countries: ", e);
         }
     }
+
+    public TMDBWatchProviderListResponse getWatchProvidersForMovies(String region) {
+        log.info("Start getting watch providers for movies with region={}", region);
+        try {
+            TMDBWatchProviderListResponse tmdbWatchProviderListResponse = TMDBClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/watch/providers/movie")
+                            .queryParam("language", "en-US")
+                            .queryParamIfPresent("watch_region", Optional.ofNullable(region))
+                            .build()
+                    )
+                    .retrieve()
+                    .body(TMDBWatchProviderListResponse.class);
+
+            tmdbWatchProviderListResponse.getProviders().sort(
+                    (o1, o2) -> o1.getDisplayPriority().compareTo(o2.getDisplayPriority())
+            );
+
+            return TMDBWatchProviderListResponse.builder()
+                    .providers(
+                            tmdbWatchProviderListResponse.getProviders()
+                    )
+                    .total(tmdbWatchProviderListResponse.getProviders().size())
+                    .build();
+
+        } catch (Exception e) {
+            log.error("getting watch providers for movies with region:{} ", region, e);
+            throw new RuntimeException("Failed to get countries: ", e);
+        }
+    }
+
+    public TMDBWatchProviderListResponse getWatchProvidersForTvShows(String region) {
+        log.info("Start getting watch providers for movies with region={}", region);
+        try {
+            TMDBWatchProviderListResponse tmdbWatchProviderListResponse = TMDBClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/watch/providers/tv")
+                            .queryParam("language", "en-US")
+                            .queryParamIfPresent("watch_region", Optional.ofNullable(region))
+                            .build()
+                    )
+                    .retrieve()
+                    .body(TMDBWatchProviderListResponse.class);
+
+            tmdbWatchProviderListResponse.getProviders().sort(
+                    (o1, o2) -> o1.getDisplayPriority().compareTo(o2.getDisplayPriority())
+            );
+
+            return TMDBWatchProviderListResponse.builder()
+                    .providers(
+                            tmdbWatchProviderListResponse.getProviders()
+                    )
+                    .total(tmdbWatchProviderListResponse.getProviders().size())
+                    .build();
+
+        } catch (Exception e) {
+            log.error("getting watch providers for movies with region:{} ", region, e);
+            throw new RuntimeException("Failed to get countries: ", e);
+        }
+    }
+
 }
