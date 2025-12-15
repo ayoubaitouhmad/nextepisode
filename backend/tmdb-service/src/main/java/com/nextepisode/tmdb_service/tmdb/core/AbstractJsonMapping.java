2c611aefbad24f9bf2cc47ee826d368997a777ba
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-15 16:42:25 +0100

    add(tmdb): add base classes for clean up infrastructure

---
+++ backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/tmdb/core/AbstractJsonMapping.java
@@ -0,0 +1,40 @@
+package com.nextepisode.tmdb_service.tmdb.core;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/***
+ * This class (AbstractJsonMapping) is derived from ProjectName,c-eg/themoviedbapi
+ * originally found at: https://github.com/c-eg/themoviedbapi/blob/master/src/main/java/info/movito/themoviedbapi/model/core/AbstractJsonMapping.java#L14
+ * Original Author: [Holger Brandl]
+ */
+@NoArgsConstructor
+@Data
+@SuperBuilder
+public abstract class AbstractJsonMapping implements Serializable {
+
+
+    private final Map<String, Object> newItems = new HashMap<>();
+
+    /**
+     * Gets the new items that were not mapped to any field from a model extending this class.
+     *
+     * @return the new items.
+     */
+    @JsonAnyGetter
+    public Map<String, Object> getNewItems() {
+        return newItems;
+    }
+
+    @JsonAnySetter
+    public void setNewItems(String name, Object value) {
+        newItems.put(name, value);
+    }
+}
