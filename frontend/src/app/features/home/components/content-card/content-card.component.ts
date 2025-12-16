d87cb060da66ae0b4a696ab79bfefdaba51c7427
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-16 20:06:05 +0100

    refactor(frontend): comment unused code

--- frontend/src/app/features/home/components/content-card/content-card.component.ts
+++ frontend/src/app/features/home/components/content-card/content-card.component.ts
@@ -50,29 +50,29 @@ export class ContentCardComponent implements OnInit {
     this.isProcessingFavorites = true;
 
     if (this.isInFavorites) {
-      this.userMovieService.removeFromFavorites(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInFavorites = false;
-          this.addClickAnimation('favorite');
-          this.isProcessingFavorites = false;
-        },
-        error: (error) => {
-          console.error('Error removing from favorites:', error);
-          this.isProcessingFavorites = false;
-        }
-      });
+      // this.userMovieService.removeFromFavorites(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInFavorites = false;
+      //     this.addClickAnimation('favorite');
+      //     this.isProcessingFavorites = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error removing from favorites:', error);
+      //     this.isProcessingFavorites = false;
+      //   }
+      // });
     } else {
-      this.userMovieService.addToFavorites(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInFavorites = true;
-          this.addClickAnimation('favorite');
-          this.isProcessingFavorites = false;
-        },
-        error: (error) => {
-          console.error('Error adding to favorites:', error);
-          this.isProcessingFavorites = false;
-        }
-      });
+      // this.userMovieService.addToFavorites(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInFavorites = true;
+      //     this.addClickAnimation('favorite');
+      //     this.isProcessingFavorites = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error adding to favorites:', error);
+      //     this.isProcessingFavorites = false;
+      //   }
+      // });
     }
     this.addToFavorites.emit(this.content as XMovie);
   }
@@ -86,29 +86,29 @@ export class ContentCardComponent implements OnInit {
     this.isProcessingWatched = true;
 
     if (this.isInWatched) {
-      this.userMovieService.removeFromWatched(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInWatched = false;
-          this.addClickAnimation('watched');
-          this.isProcessingWatched = false;
-        },
-        error: (error) => {
-          console.error('Error removing from watched:', error);
-          this.isProcessingWatched = false;
-        }
-      });
+      // this.userMovieService.removeFromWatched(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInWatched = false;
+      //     this.addClickAnimation('watched');
+      //     this.isProcessingWatched = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error removing from watched:', error);
+      //     this.isProcessingWatched = false;
+      //   }
+      // });
     } else {
-      this.userMovieService.addToWatched(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInWatched = true;
-          this.addClickAnimation('watched');
-          this.isProcessingWatched = false;
-        },
-        error: (error) => {
-          console.error('Error adding to watched:', error);
-          this.isProcessingWatched = false;
-        }
-      });
+      // this.userMovieService.addToWatched(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInWatched = true;
+      //     this.addClickAnimation('watched');
+      //     this.isProcessingWatched = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error adding to watched:', error);
+      //     this.isProcessingWatched = false;
+      //   }
+      // });
     }
     this.addToWatched.emit(this.content as XMovie);
   }
@@ -122,29 +122,29 @@ export class ContentCardComponent implements OnInit {
     this.isProcessingWatchlist = true;
 
     if (this.isInWatchlist) {
-      this.userMovieService.removeFromWatchlist(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInWatchlist = false;
-          this.addClickAnimation('watchlist');
-          this.isProcessingWatchlist = false;
-        },
-        error: (error) => {
-          console.error('Error removing from watchlist:', error);
-          this.isProcessingWatchlist = false;
-        }
-      });
+      // this.userMovieService.removeFromWatchlist(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInWatchlist = false;
+      //     this.addClickAnimation('watchlist');
+      //     this.isProcessingWatchlist = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error removing from watchlist:', error);
+      //     this.isProcessingWatchlist = false;
+      //   }
+      // });
     } else {
-      this.userMovieService.addToWatchlist(parseInt(this.content.id)).subscribe({
-        next: () => {
-          this.isInWatchlist = true;
-          this.addClickAnimation('watchlist');
-          this.isProcessingWatchlist = false;
-        },
-        error: (error) => {
-          console.error('Error adding to watchlist:', error);
-          this.isProcessingWatchlist = false;
-        }
-      });
+      // this.userMovieService.addToWatchlist(parseInt(this.content.id)).subscribe({
+      //   next: () => {
+      //     this.isInWatchlist = true;
+      //     this.addClickAnimation('watchlist');
+      //     this.isProcessingWatchlist = false;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error adding to watchlist:', error);
+      //     this.isProcessingWatchlist = false;
+      //   }
+      // });
     }
     this.addToWatchlist.emit(this.content as XMovie);
   }
@@ -157,16 +157,16 @@ export class ContentCardComponent implements OnInit {
   private checkUserLists(): void {
     // Check if movie is in user's lists by calling the backend API
     if (this.content && 'id' in this.content) {
-      this.userMovieService.checkMovieStatus(parseInt(this.content.id)).subscribe({
-        next: (status: any) => {
-          this.isInFavorites = status.isFavorite;
-          this.isInWatched = status.isWatched;
-          this.isInWatchlist = status.isInWatchlist;
-        },
-        error: (error) => {
-          console.error('Error checking movie status:', error);
-        }
-      });
+      // this.userMovieService.checkMovieStatus(parseInt(this.content.id)).subscribe({
+      //   next: (status: any) => {
+      //     this.isInFavorites = status.isFavorite;
+      //     this.isInWatched = status.isWatched;
+      //     this.isInWatchlist = status.isInWatchlist;
+      //   },
+      //   error: (error) => {
+      //     console.error('Error checking movie status:', error);
+      //   }
+      // });
     }
   }
 
