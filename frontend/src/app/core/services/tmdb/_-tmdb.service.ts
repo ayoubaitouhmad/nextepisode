d65d1062208f062ec2730e19ba95ef2c674a12c0
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-18 14:20:09 +0100

    feat(frontend): cache the movie/tv shows genres

--- frontend/src/app/core/services/tmdb/_-tmdb.service.ts
+++ frontend/src/app/core/services/tmdb/_-tmdb.service.ts
@@ -1,8 +1,10 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Service} from './service';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {GenreList, LanguageList, PersonList, RegionList, WatchProviderList} from '../../models/common/shared-dtos';
+import {LocalStorageCacheService} from '../local-storage-cache.service';
+import {tap} from 'rxjs/operators';
 
 
 @Injectable({
@@ -10,26 +12,45 @@ import {GenreList, LanguageList, PersonList, RegionList, WatchProviderList} from
 })
 export class _TmdbService extends Service {
 
+  public static readonly MOVIE_GENRE_CACHE_KEY = `movie-genres`;
+  public static readonly TV_SHOW_CACHE_KEY = `tv-show-genres`;
   protected apiUrl: string;
 
-  constructor(http: HttpClient) {
+  constructor(http: HttpClient, private localCache: LocalStorageCacheService) {
     super(http);
     this.apiUrl = `${this.baseServiceApiUrl}`;
   }
-
 
   /**
    * Get current user profile
    */
   getMoviesGenres(): Observable<GenreList> {
-    return this.http.get<GenreList>(`${this.apiUrl}/genres/movie-genres`);
+    const path = `/genres/movie-genres`;
+    return this.fetchGenres(path, _TmdbService.MOVIE_GENRE_CACHE_KEY)
   }
 
   /**
    * Get current user profile
    */
   getTvShowsGenres(): Observable<GenreList> {
-    return this.http.get<GenreList>(`${this.apiUrl}/genres/tv-show-genres`);
+    const path = `/genres/tv-show-genres`;
+    return this.fetchGenres(path, _TmdbService.TV_SHOW_CACHE_KEY)
+  }
+
+
+  fetchGenres(path: string, cacheKey: string) {
+    const cached = this.localCache.get<GenreList>(cacheKey);
+    if (cached) {
+      return of(cached);
+    }
+    {
+      return this.http.get<GenreList>(`${this.apiUrl}${path}`).pipe(
+        tap(list => {
+          list.stored_at = new Date();
+          this.localCache.set(cacheKey, list)
+        })
+      );
+    }
   }
 
   /**
