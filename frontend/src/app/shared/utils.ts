b5c6f4d4cf47d72c690f0ad4c5d1e60b70c5c50c
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2025-12-21 15:09:53 +0100

    feat(front): add capitalizeFirstLetter helper func

--- frontend/src/app/shared/utils.ts
+++ frontend/src/app/shared/utils.ts
@@ -2,3 +2,8 @@ export function getYearFromDate(releaseDate: string): string {
   if (!releaseDate) return 'Unknown';
   return new Date(releaseDate).getFullYear().toString();
 }
+
+export function capitalizeFirstLetter(string: string) {
+  if (!string) return ""; // Handle empty strings
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
