cd7f19a144393b823a23176779aad355edb341ec
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2026-01-01 17:33:38 +0100

    feat(front): add specific movie filter

--- frontend/src/app/core/models/tmdb/request/content-filters.ts
+++ frontend/src/app/core/models/tmdb/request/content-filters.ts
@@ -1,17 +1,34 @@
 export interface ContentFilters {
   type: 'movie' | 'tv';
-  genres: number[];
-  year: number;
-  includeAdult: boolean;
+  // Optional filters that can be undefined
+  includeAdult?: boolean;
+  genres?: number[];
+  watchProviders?: number[];
+  year?: number;
   yearFrom?: number;
   yearTo?: number;
-  language: string;
-  runtime: string;
-  castAndCrew: string;
-  keyword: string;
-  sortBy: string;
-  certification: string;
-  watchProviders: number[];
-  region: string;
+  language?: string;
+  runtime?: string;
+  castAndCrew?: string;
+  keyword?: string;
+  sortBy?: string;
+  certification?: string;
+  region?: string;
 }
 
+export interface MovieFilters {
+  page?: number;
+  genres?: number[];
+  year?: number;
+  includeAdult?: boolean;
+  yearFrom?: number;
+  yearTo?: number;
+  language?: string;
+  runtime?: string;
+  castAndCrew?: string;
+  keyword?: string;
+  sortBy?: string;
+  certification?: string;
+  watchProviders?: number[];
+  region?: string;
+}
