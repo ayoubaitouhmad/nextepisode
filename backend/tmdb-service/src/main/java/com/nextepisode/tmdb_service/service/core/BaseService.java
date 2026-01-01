c1f21973eed35e56bcc5cb42a0dff24467e9e1ba
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2026-01-01 15:22:14 +0100

    feat(tmdb): add a generic get request to make get request query params and a response type

--- backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/service/core/BaseService.java
+++ backend/tmdb-service/src/main/java/com/nextepisode/tmdb_service/service/core/BaseService.java
@@ -15,4 +15,22 @@ import java.util.function.Function;
 @Slf4j
 public abstract class BaseService {
     protected final RestClient tmdbClient;
+
+    /***
+     * Generic get request for the Tmdb Rest Client
+     * @param uriFunction URI builder lambda
+     * @param responseType response class
+     * @return
+     * @param <T>
+     */
+    protected  <T> T get(
+            Function<UriBuilder, URI> uriFunction,
+            Class<T> responseType
+    ) {
+        log.debug("TMDB GET request for responseType={} with path:{}", responseType.getSimpleName() , uriFunction.toString());
+        return tmdbClient.get()
+                .uri(uriFunction)
+                .retrieve()
+                .body(responseType);
+    }
 }
