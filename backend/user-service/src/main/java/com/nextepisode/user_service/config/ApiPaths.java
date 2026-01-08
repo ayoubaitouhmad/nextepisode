f2ef8c1c88e6f991849953ef7e7447d514c96481
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2026-01-08 15:19:57 +0100

    refactor(user): rename ApiPaths.BASE to API_V1

--- backend/user-service/src/main/java/com/nextepisode/user_service/config/ApiPaths.java
+++ backend/user-service/src/main/java/com/nextepisode/user_service/config/ApiPaths.java
@@ -1,6 +1,6 @@
 package com.nextepisode.user_service.config;
 
 public class ApiPaths {
-    public static final String BASE = "/api/v1/user";
+    public static final String API_V1 = "/api/v1/user";
 
 }
