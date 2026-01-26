921962778bbe3aaa0a9983748308aeb025a01b56
AyoubAitOuhamd(Aka MRX) <ayoubaithmad77@gmail.com>
2026-01-26 12:23:31 +0100

    feature(log): make the base service class able to use the logger service

--- frontend/src/app/core/services/tmdb/service.ts
+++ frontend/src/app/core/services/tmdb/service.ts
@@ -1,16 +1,16 @@
-import {Injectable} from '@angular/core';
+import {inject, Injectable} from '@angular/core';
 import {environment} from '../../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {Logger, LoggerService} from '../logger.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+// service.ts (base class)
+@Injectable({providedIn: 'root'})
 export class Service {
-
   protected baseServiceApiUrl = `${environment.apiUrl}/tmdb`;
+  protected loggerService = inject(LoggerService);
+  protected logger: Logger;
 
   constructor(protected http: HttpClient) {
+    this.logger = this.loggerService.create('Service');
   }
-
-
 }
